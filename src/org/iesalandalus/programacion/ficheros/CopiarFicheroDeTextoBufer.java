a31be2847014a24a43515d8e5b6ad043916bdea9
Juanmi <juanmi-64@hotmail.com>
2024-04-07 10:58:43 +0200

    Cambios 07/04/2024

--- src/org/iesalandalus/programacion/ficheros/CopiarFicheroDeTextoBufer.java
+++ src/org/iesalandalus/programacion/ficheros/CopiarFicheroDeTextoBufer.java
@@ -4,18 +4,19 @@ import org.iesalandalus.programacion.utilidades.Entrada;
 
 import java.io.*;
 
-public class CopiarFicheroDeTextoFlujo {
+public class CopiarFicheroDeTextoBufer {
     private static final String FICHERO = String.format("%s%s%s", "ejemplos", File.separator, "ficheroTexto.txt");
 
     public static void main(String[] args) {
         System.out.printf("%nDime el nombre del fichero que quieres guardar como copia: ");
         String ficheroNuevo = Entrada.cadena();
 
-        try (FileWriter ficheroCopia = new FileWriter(ficheroNuevo);
-             FileReader entrada = new FileReader(FICHERO)) {
-            int origen;
-            while ((origen = entrada.read()) != -1) {
-            ficheroCopia.write(((char) origen));
+        try (BufferedWriter ficheroCopia = new BufferedWriter(new FileWriter(ficheroNuevo));
+             BufferedReader entrada = new BufferedReader(new FileReader(FICHERO))) {
+            String origen;
+            while ((origen = entrada.readLine()) != null) {
+            ficheroCopia.write(origen);
+            ficheroCopia.newLine();
             }
             System.out.println("Fichero copiado.");
 
--- src/org/iesalandalus/programacion/ficheros/MostrarFicheroDeTextoBufer.java
+++ src/org/iesalandalus/programacion/ficheros/MostrarFicheroDeTextoBufer.java
@@ -1,20 +1,16 @@
 package org.iesalandalus.programacion.ficheros;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class MostrarFicheroDeTextoBufer {
     private static final String FICHERO = String.format("%s%s%s", "ejemplos", File.separator, "ficheroTexto.txt");
 
     public static void main(String[] args) {
-        try (FileReader entrada = new FileReader(FICHERO)) {
-            int leido;
-            while ((leido = entrada.read()) != -1) {
-                System.out.print(((char) leido));
+        try (BufferedReader entrada = new BufferedReader(new FileReader(FICHERO))) {
+            String leido;
+            while ((leido = entrada.readLine()) != null) {
+                System.out.printf("%s%n", leido);
             }
-            System.out.println();
         } catch (FileNotFoundException e) {
             System.out.println("No se puede leer el fichero.");
         } catch (IOException e) {
